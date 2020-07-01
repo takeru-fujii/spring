0eae275b69a18c845143a1c0f7ba83ffbcea571b
root <root@v157-7-139-75.myvps.jp>
2020-07-01 20:40:06 +0900

    add kotlin spring boot

---
+++ src/test/kotlin/com/rutake/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rutake.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
